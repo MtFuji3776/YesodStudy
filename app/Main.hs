4c54c038f551fe8a7205ed12ae3c606025591f8a
mtfuji3776 <32.allegro.con.brio.ed.appassionato@gmail.com>
2020-10-11 00:07:12 +0900

    エラー解消。THのせいか分からんがparseRoutesの後にスペースが挟まってると無関係な場所にエラーが出るので注意。チュートリアル達成。

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+
 module Main where
 
+import Yesod
 import Lib
 
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes| / HomeR GET |] -- parseRoutesと|の間にスペースを入れるべからず！！関係ないところにエラーが出る！
+
+instance Yesod HelloWorld
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Hello World!|]
+
 main :: IO ()
-main = someFunc
+main = warp 3000 HelloWorld
